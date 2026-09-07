4a1ae6b9cd45c225edc4ab1efb0b521a7fefdb0b
Philippe Doucet <philippe.doucet@inspectify.com>
2026-09-07 10:51:57 -0400

    moved todo to todo file

--- src/types/event.ts
+++ src/types/event.ts
@@ -4,6 +4,4 @@ export type Event = {
   id: ID;
   name: string;
   year: number;
-  // TODO: Consider adding a Date object for more precise date handling (invalid dates for very ancient events)
-  // date: Date;
 };
